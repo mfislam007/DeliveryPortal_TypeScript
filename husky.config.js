ca0787098e82f3b790c990eafd92cb4abaeefd25
roman-bezusiak <bezusiakroman@gmail.com>
2020-06-15 15:40:23 +0300

    Deleted redundant Prettier configuration

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@
  * Concatenates string[] of a shell command and its arguments
  * via ` `-chaining
  */
-const cmd = (a) => {
+const cmd = a => {
   if (a instanceof Array && a.join) return a.join(" ");
   throw TypeError;
 };
@@ -11,18 +11,16 @@ const cmd = (a) => {
  * Concatenates string[] of shell commands into one string via
  * `&&`-chaining
  */
-const cmds = (a) => {
+const cmds = a => {
   if (a instanceof Array && a.join) return a.join(" && ");
   throw TypeError;
 };
 
 module.exports = {
   hooks: {
-    "pre-commit": cmds([
-      cmd(["pretty-quick", "--arrow-parens avoid", "--staged"]),
-    ]),
+    "pre-commit": cmds([cmd(["pretty-quick", "--staged"])]),
     "pre-push": cmds([
-      cmd(["pretty-quick", "--arrow-parens avoid", "--staged"]),
+      cmd(["pretty-quick", "--staged"]),
       cmd([
         "yarn jest",
         "--bail 1", // Exits on 1 failed test
