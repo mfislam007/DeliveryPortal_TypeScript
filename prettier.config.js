eef6ebd035fded00e54a5ac5937395b6d75c6bc6
roman-bezusiak <bezusiakroman@gmail.com>
2020-06-15 15:38:47 +0300

    Added Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  quoteProps: "as-needed",
+  endOfLine: "lf",
+};
