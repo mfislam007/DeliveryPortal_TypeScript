d93b89b50f86846c3ad4e19363ef8020bcffe8fa
roman-bezusiak <bezusiakroman@gmail.com>
2020-06-16 00:04:32 +0300

    Finished MVP webpack configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./util/serviceWorker";
 
+// These imports are required for `index.html` webpack bundling
+require("./assets/images/favicon.ico");
+require("./assets/images/logo192.png");
+require("./assets/images/logo512.png");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -13,4 +18,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
---
+++ webpack.dev.js
@@ -0,0 +1,50 @@
+/**
+ * This is a webpack configuration file for development build
+ *  BUG (Roman Bezusiak) [ Image loading doesn't work in `developer` mode, probable cause: absence of native webpack mapping ]
+ */
+
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+
+  module: {
+    rules: [
+      // CSS loading rule
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: [
+          "style-loader", // Injects styles into DOM
+          "css-loader", // Loads CSS
+        ],
+      },
+
+      // SASS loading rule
+      {
+        test: /\.scss$/i,
+        include: path.resolve(__dirname, "src"),
+        use: [
+          "style-loader", // Injects styles into DOM
+          "css-loader", // Loads CSS
+          "sass-loader", // Converts SASS into CSS
+        ],
+      },
+    ],
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+  },
+
+  plugins: [
+    // Auto-generates the `index.html` based on the existing template
+    // with the up-to-date JS bundle import
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+});
