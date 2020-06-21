66843bb9f9619b33447aeabac4fc5c8ac90cff8c
Mr Ekseli <tka@vamk.fi>
2020-06-21 13:48:32 +0300

    Fetches list of projects from Niko's POD

--- src/containers/ProjectList.tsx
+++ src/containers/ProjectList.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ProviderLogin } from "@inrupt/solid-react-components";
-import { LoggedIn, LoggedOut, LogoutButton } from "@solid/react";
+import { LoggedIn, LoggedOut, LogoutButton, List } from "@solid/react";
 
 const ProjectList: React.FC<Props> = (props): JSX.Element => {
   return (
@@ -13,6 +13,12 @@ const ProjectList: React.FC<Props> = (props): JSX.Element => {
       </LoggedOut>
       <LoggedIn>
         <span>Hello projects</span>
+        {/* @TODO move data from Nikos POD */}
+        <List
+          src={`
+                  ["https://nikosiltaloppi.inrupt.net/profile/card#me"].blog
+                    .schema_blogPost.label`}
+        />
       </LoggedIn>
     </div>
   );
