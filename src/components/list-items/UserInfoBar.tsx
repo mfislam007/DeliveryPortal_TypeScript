e6fb9693517698efd1f64373b917068593754a38
Antti Välimaa <e1700798@edu.vamk.fi>
2020-06-26 09:52:16 +0300

    #274 User info bar component for project users page.

---
+++ src/components/list-items/UserInfoBar.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import moreOptions from "../../assets/images/commonicons/options-icon.png";
+import defaultPortrait from "../../assets/images/commonicons/portrait-icon.png";
+import "./UserInfoBar.css";
+
+type Props = {
+  name: string;
+  company: string;
+  portrait?: string;
+};
+
+const UserInfoBar: React.FC<Props> = (props: Props): JSX.Element => {
+  const [name, setName] = useState<string>("");
+  const [company, setCompany] = useState<string>("");
+  const [portrait, setPortrait] = useState<string>();
+
+  useEffect(() => {
+    setName(props.name);
+    setCompany(props.company);
+    if (props.portrait !== undefined) {
+      setPortrait(props.portrait);
+    }
+  }, [props]);
+
+  return (
+    <div className="userInfoBarContainer">
+      <div className="userInfoBarPortrait">
+        <img
+          src={portrait === undefined ? defaultPortrait : portrait}
+          alt="portrait"
+          width={30}
+          height={30}
+        />
+      </div>
+      <div className="userInfoBarName">{name}</div>
+      <div className="userInfoBarCompany">{company}</div>
+      <div className="userInfoBarSettingsImg">
+        <img src={moreOptions} alt="settings" width={25} height={25} />
+      </div>
+    </div>
+  );
+};
+
+export default UserInfoBar;
