7e2d0ae37eb1c23a87f3f7a3dcc7157cba8fb778
hindia <hindia.mahamoed@fineto.fi>
2020-06-30 13:49:14 +0300

    #311 styled the side drawer

--- src/components/menubar/SideDrawer/SideDrawer.tsx
+++ src/components/menubar/SideDrawer/SideDrawer.tsx
@@ -7,7 +7,7 @@ import calender1 from "../../../assets/images/menubar/calender1.png";
 import setting1 from "../../../assets/images/menubar/setting1.png";
 import dp1 from "../../../assets/images/menubar/news1.png";
 
-const sideDrawer = props => {
+const sideDrawer = (props: any) => {
   let drawerClasses = "side-drawer";
   if (props.show) {
     drawerClasses = "side-drawer open";
@@ -16,44 +16,25 @@ const sideDrawer = props => {
     <nav className={drawerClasses}>
       <ul>
         <li>
-          <a href="/">
-            <img src={delivery1} alt="delivery1" className="logo" />
-            Delivery portals
-          </a>
+          <a href="/">Delivery portals</a>
         </li>
         <li>
-          <a href="/">
-            <img src={news1} alt="news1" className="logo" />
-            News
-          </a>
+          <a href="/">News</a>
         </li>
         <li>
-          <a href="/">
-            <img src={calender1} alt="calender1" className="logo" />
-            Calender
-          </a>
+          <a href="/">Calender</a>
         </li>
 
         <li>
-          <a href="/">
-            <img src={setting1} alt="setting1" className="logo" />
-            Setting
-          </a>
+          <a href="/">Setting</a>
         </li>
-        <div className="spacer2"></div>
-        <div className="addbar2">
-          <li>
-            <a href="/">
-              <img src={dp1} alt="dp1" className="logo" /> Join Delivery portal
-            </a>
-          </li>
+        <li>
+          <a href="/">Join Delivery portal</a>
+        </li>
 
-          <li>
-            <a href="/">
-              <img src={dp1} alt="dp1" className="logo" /> Add Delivery portal
-            </a>
-          </li>
-        </div>
+        <li>
+          <a href="/">Add Delivery portal</a>
+        </li>
       </ul>
     </nav>
   );
