90aaeff5fcea5e44d39639677d438b64c8dfc82f
Antti Välimaa <e1700798@edu.vamk.fi>
2020-06-30 17:56:37 +0300

    #274 Removed 'workaround

--- src/components/navigation/ProjectPageSwitch.tsx
+++ src/components/navigation/ProjectPageSwitch.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { Switch, Route, useRouteMatch, Link } from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import ProjectIndex from "../pages/deliveryphase/project/ProjectStream";
 import ProjectPhases from "../pages/deliveryphase/project/ProjectPhases";
 import ProjectUsers from "../pages/deliveryphase/project/ProjectUsers";
 
 const ProjectCardSwitch: React.FC = (): JSX.Element => {
-  const { url, path } = useRouteMatch();
+  const { path } = useRouteMatch();
 
   return (
-    <div>
-      <Link to={url}>Stream_</Link>
-      <Link to={url + "/phase"}>Phase_</Link>
-      <Link to={url + "/users"}>Users_</Link>
-      <Switch>
-        <Route path={path + "/users"} component={ProjectUsers}></Route>
-        <Route path={path + "/phase"} component={ProjectPhases}></Route>
-        <Route path={path} component={ProjectIndex}></Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path={path + "/users"} component={ProjectUsers}></Route>
+      <Route path={path + "/phase"} component={ProjectPhases}></Route>
+      <Route path={path} component={ProjectIndex}></Route>
+    </Switch>
   );
 };
 
