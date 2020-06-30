614570c7973b24f9a7cfd6be63c95479ade3a564
Mr Ekseli <tka@vamk.fi>
2020-06-30 20:48:34 +0300

    Fetch phases from POD using temporarely ProjectCard for rendering

--- src/components/pages/deliveryphase/project/ProjectPhases.tsx
+++ src/components/pages/deliveryphase/project/ProjectPhases.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../DeliveryPhaseIndex.css";
+import { fetchDocument } from "tripledoc";
+import ProjectCard from "../../../list-items/ProjectCard";
 
 const ProjectPhases: React.FC = (): JSX.Element => {
-  const { id } = useParams();
-  return <div id={id}>"Phase"-page for project {id}.</div>;
+  const [names, setNames] = useState<string[]>([]);
+  useEffect(() => {
+    /** Should pass the webid and project name as argument. To list the phases user must have read access to POD */
+    getNames("https://ekseli.dev.inrupt.net/private/dp/cases/Project2/project.ttl");
+    async function getNames(webId: string) {
+      const profileDoc = await fetchDocument(webId);
+      const profile = profileDoc.getSubject(webId);
+      console.log(JSON.stringify(profile.getAllStrings("https://schema.org/Action#name")));
+      setNames(profile.getAllStrings("https://schema.org/Action#name"));
+    }
+  }, []);
+
+  const nameCards = names.map((name, index) => (
+    <ProjectCard
+      id={index}
+      key={index}
+      title={name}
+      owner="This is a phase but uses ProjectCards for visualization"
+      tags={[
+        { id: 1, name: "SGe" },
+        { id: 2, name: "Car Industry" },
+      ]}
+    ></ProjectCard>
+  ));
+  return <div className="deliveryPortalProjectsContainer">{nameCards}</div>;
 };
 
 export default ProjectPhases;
