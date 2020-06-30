846e3a7e841fa8c3134084e88bbd0d1a0ca9624c
Antti Välimaa <e1700798@edu.vamk.fi>
2020-06-30 17:49:44 +0300

    #274 Added menubar for the app.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import HomePageSwitch from "./navigation/HomePageSwitch";
 import "./App.css";
+import MenuBar from "./menubar/MenuBar";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="App">
       <Router>
+        <MenuBar />
         <HomePageSwitch />
       </Router>
     </div>
