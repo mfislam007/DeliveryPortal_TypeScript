1905865b00500a0ee00f8ffaf74b29cfa0b07e12
Mr Ekseli <tka@vamk.fi>
2020-06-30 19:54:33 +0300

    Fixed to fetch the project names from POD

--- src/components/pages/deliveryphase/DeliveryPhaseIndex.tsx
+++ src/components/pages/deliveryphase/DeliveryPhaseIndex.tsx
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProjectCard from "../../list-items/ProjectCard";
 import "./DeliveryPhaseIndex.css";
-
+import "regenerator-runtime/runtime";
+import { fetchDocument } from "tripledoc";
 const DeliveryPhaseIndex: React.FC = (): JSX.Element => {
+  const [names, setNames] = useState<string[]>([]);
+  /**
+   * Read project names from POD
+   */
+  useEffect(() => {
+    getNames("https://ekseli.dev.inrupt.net/private/dp/cases/Project2/project.ttl");
+    async function getNames(webId: string) {
+      const profileDoc = await fetchDocument(webId);
+      const profile = profileDoc.getSubject(webId);
+      console.log(JSON.stringify(profile.getAllStrings("http://schema.org/Project#name")));
+      setNames(profile.getAllStrings("http://schema.org/Project#name"));
+    }
+  }, []);
+  /**
+   * loop here names array and create ProejctCards for them
+   */
+  const nameCards = names.map((name, index) => (
+    <ProjectCard
+      id={index}
+      title={name}
+      owner="Esko Petäjä"
+      tags={[
+        { id: 1, name: "SGe" },
+        { id: 2, name: "Car Industry" },
+      ]}
+    ></ProjectCard>
+  ));
   return (
     <div>
       <h3>Delivery Portals</h3>
-      <div className="deliveryPortalProjectsContainer">
-        <ProjectCard
-          id={1}
-          title="Audi's DP"
-          owner="Esko Petäjä"
-          tags={[
-            { id: 1, name: "SGe" },
-            { id: 2, name: "Car Industry" },
-          ]}
-        />
-        <ProjectCard
-          id={2}
-          title="ABB's DP"
-          owner="Esko Petäjä"
-          tags={[
-            { id: 1, name: "SGe" },
-            { id: 2, name: "Car Industry" },
-          ]}
-        />
-      </div>
+      <div className="deliveryPortalProjectsContainer">{nameCards}</div>
     </div>
   );
 };
-
 export default DeliveryPhaseIndex;
