84c5a988b6fbe2c5177ad631686a586567d646a9
Niko Siltaloppi <niko.siltaloppi@gmail.com>
2020-07-01 13:15:25 +0300

    code cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,7 @@
 import React from "react";
-import { LoggedIn, LoggedOut, LogoutButton } from "@solid/react";
-import { ProviderLogin } from "@inrupt/solid-react-components";
-import ImageList from "../containers/ImageList/ImageList";
 
 const App: React.FC = (): JSX.Element => {
-  return (
-    <div>
-      <LoggedOut>
-        <ProviderLogin callbackUri={`${window.location.origin}/`} />
-      </LoggedOut>
-      <LoggedIn>
-        <ImageList dataSource="https://nikosiltaloppi.solid.community/private/pictures/" />
-        <LogoutButton />
-      </LoggedIn>
-    </div>
-  );
+  return <div>Project template</div>;
 };
 
 export default App;
--- webpack.common.js
+++ webpack.common.js
@@ -42,7 +42,6 @@ module.exports = {
       {
         test: /\.m?js$/i,
         enforce: "pre",
-        exclude: /node_modules/,
         loader: "source-map-loader",
       },
 
