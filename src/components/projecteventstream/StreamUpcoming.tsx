6aaff72d1e1e0c09321ec00ac3d9e959100d1627
Antti Välimaa <e1700798@edu.vamk.fi>
2020-07-01 19:58:10 +0300

    #274 Fixed a typo.

--- src/components/projecteventstream/StreamUpcoming.tsx
+++ src/components/projecteventstream/StreamUpcoming.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import "./StreamElement.css";
-import calenderIcon from "../../assets/images/commonicons/calender-icon.png";
+import calendarIcon from "../../assets/images/commonicons/calender-icon.png";
 
 const StreamUpcoming: React.FC = (): JSX.Element => {
   return (
     <div className="projectStreamEventUpcomingMain">
       <h3>Upcoming</h3>
       <p>No Tasks due soon! Check your calender for more.</p>
-      <img src={calenderIcon} alt="calender"></img>
+      <img src={calendarIcon} alt="calendar"></img>
     </div>
   );
 };
