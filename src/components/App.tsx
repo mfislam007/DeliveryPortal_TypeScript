d28489f0ca6a1d38ea415004cd402b0d3363d935
Md Farukul Islam <mfislam007@yahoo.com>
2020-07-02 20:37:19 +0300

    #204 First commit on a modal window to add a project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
-import "./App.css";
-import HomePageSwitch from "./navigation/HomePageSwitch";
-import MenuBar from "./menubar/MenuBar";
 import AddProject from "./Modal/AddProject";
 
 const App: React.FC = (): JSX.Element => {
