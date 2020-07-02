3287b62dd318a81de42cc1a41875cd5b09cc21e3
hindia <hindia.mahamoed@fineto.fi>
2020-07-02 14:37:32 +0300

    #311 feature: add plus icon

--- src/components/menubar/MenuBackdrop/DropDownButton.tsx
+++ src/components/menubar/MenuBackdrop/DropDownButton.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import "./DropDownButton.css";
 
+import plusIcon from "../../../assets/images/menubar/plus-icon.png";
+import plusIconClicked from "../../../assets/images/menubar/plus-icon-clicked.png";
+
 type Props = {
   onClick: React.MouseEventHandler<HTMLElement>;
 };
 
 const DropDownButton: React.FC<Props> = (props): JSX.Element => (
   <div>
-    <button className="addbar" onClick={props.onClick}>
-      +
+    <button className="plus-icon" onClick={props.onClick}>
+      <img src={plusIcon} alt="plus" className="plus-icon" />
     </button>
   </div>
 );
