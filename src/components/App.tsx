b343fd6f0ed4bed1d8018eafc52e07a2595db5f7
tka <tka@puv.fi>
2020-07-08 12:52:09 +0300

    Added TextField

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import EditPhase from "../components/pages/deliveryphase/project/EditPhase";
 const App: React.FC = (): JSX.Element => {
+  const phase = "https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Preinstallation";
   const s = new Date(2020, 5, 1);
   const e = new Date(2020, 6, 15);
   return (
     <div className="App">
-      <EditPhase start={s} end={e} />
+      <EditPhase phase={phase} start={s} end={e} />
     </div>
   );
 };
--- src/components/pages/deliveryphase/project/EditPhase.tsx
+++ src/components/pages/deliveryphase/project/EditPhase.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-
+import "date-fns";
+import TextField from "@material-ui/core/TextField";
+import Modal from "react-modal";
 interface Props {
+  phase: string;
   start: Date;
   end: Date;
 }
 const EditPhase: React.FC<Props> = (props): JSX.Element => {
-  return <div>Edit phases</div>;
+  return (
+    <div>
+      <h1>Edit phases</h1>
+      <Modal isOpen={true}>
+        <TextField id="phase" value={props.phase} label="Phase" />
+      </Modal>
+    </div>
+  );
 };
 
 export default EditPhase;
