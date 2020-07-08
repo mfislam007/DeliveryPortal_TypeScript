e845e1d12e4487f9599ff3485ff76bd0efaa17fc
Mr Ekseli <tka@vamk.fi>
2020-07-08 20:43:04 +0300

    Updated landing page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import EditPhase from "../components/pages/deliveryphase/project/EditPhase";
+import MenuBar from "./menubar/MenuBar";
+import HomePageSwitch from "./navigation/HomePageSwitch";
+import { Router } from "react-router-dom";
 const App: React.FC = (): JSX.Element => {
   const phase = "https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Preinstallation";
   const s = new Date(2020, 5, 1);
   const e = new Date(2020, 6, 15);
   return (
     <div className="App">
-      <EditPhase phase={phase} start={s} end={e} />
+      <Router>
+        <MenuBar />
+        <HomePageSwitch />
+      </Router>
     </div>
   );
 };
