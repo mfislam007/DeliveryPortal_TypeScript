4dc273cc1df35dc37d9e7c2888d9a04a2b6684d7
Mr Ekseli <tka@vamk.fi>
2020-07-08 15:23:27 +0300

    Added end date and splitter just phase name

--- src/components/pages/deliveryphase/project/EditPhase.tsx
+++ src/components/pages/deliveryphase/project/EditPhase.tsx
@@ -17,23 +17,58 @@ interface Props {
 /**This modal component is used to adjust phase start and end time
  * @See https://github.com/mui-org/material-ui-pickers/issues/1440
  */
+
 const EditPhase: React.FC<Props> = (props): JSX.Element => {
-  const [selectedDate, setSelectedDate] = React.useState(new Date("2014-08-18T21:11:54"));
+  const [startDate, setStartDate] = React.useState(props.start);
+  const [endDate, setEndDate] = React.useState(props.end);
+
+  const handleDateChange = date => {
+    setStartDate(date);
+  };
+
+  const parsePhaseName = url => {
+    return url.substring(url.lastIndexOf("/") + 1, url.length);
+  };
+
+  const handleEndDateChange = date => {
+    if (endDate.getMilliseconds() > startDate.getMilliseconds()) {
+      setEndDate(date);
+    } else {
+      alert("Start date cannot be later than end date");
+    }
+  };
+
   return (
     <div>
       <h1>Edit phases</h1>
       <Modal isOpen={true}>
         <Grid container justify="space-around">
-          <TextField id="phase" value={props.phase} label="Phase" />
+          <p>
+            <TextField id="phase" value={parsePhaseName(props.phase)} label="Phase" />
+          </p>
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <KeyboardDatePicker
               disableToolbar
               variant="inline"
-              format="MM/dd/yyyy"
+              format="yyyy-MM-dd"
               margin="normal"
               id="date-picker-inline"
-              label="Date picker inline"
-              value={selectedDate}
+              label="Start date"
+              value={startDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="yyyy-MM-dd"
+              margin="normal"
+              id="date-picker-inline"
+              label="End date"
+              value={endDate}
+              onChange={handleEndDateChange}
               KeyboardButtonProps={{
                 "aria-label": "change date",
               }}
