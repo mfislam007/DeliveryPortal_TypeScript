c6d5feb6b75c6d9c89b562cd42c096e9ed316fa2
Mr Ekseli <tka@vamk.fi>
2020-07-09 17:54:24 +0300

    Read date and uses App.tsx to echo it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { getPhaseDate } from "./pages/deliveryphase/project/PhaseController";
+import { getPhaseDate } from "../controllers/ProjectController/PhaseController";
 import "./App.scss";
-import HomePageSwitch from "./navigation/HomePageSwitch";
-import MenuBar from "./menubar/MenuBar";
-import EditPhase from "./pages/deliveryphase/project/EditPhase";
 
 const App: React.FC = (): JSX.Element => {
   const [start, setStart] = React.useState<Date>(new Date());
