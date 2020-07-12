56704cca6128ff9c1e48a9882be7dd7ccd91e1d0
Mr Ekseli <tka@vamk.fi>
2020-07-12 13:11:17 +0300

    Added test cases for EditPhase

--- src/components/pages/__tests__/EditPhase.test.tsx
+++ src/components/pages/__tests__/EditPhase.test.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { configure, shallow } from "enzyme";
+import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { act } from "react-dom/test-utils";
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
 import EditPhase from "../deliveryphase/project/EditPhase";
-import { createRender, createShallow } from "@material-ui/core/test-utils";
+import { createRender, createShallow, createMount } from "@material-ui/core/test-utils";
 
 configure({ adapter: new Adapter() });
 describe('Testing EditPhase component")', () => {
@@ -24,18 +25,21 @@ describe('Testing EditPhase component")', () => {
 
   beforeAll(() => {
     shallow = createShallow();
-    render = createRender();
+    render = createMount();
   });
   //https://stackoverflow.com/questions/50916374/material-ui-enzyme-testing-component
   it("Can render EditPhase", () => {
-    const wrapper2 = render(
-      <EditPhase
-        phase="https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Installation/"
-        start={new Date(2020, 1, 1)}
-        end={new Date(2020, 2, 2)}
-        open={true}
-        toggle={jest.fn()}
-      />
-    );
+    act(() => {
+      const wrapper2 = render(
+        <EditPhase
+          phase="https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Installation/"
+          start={new Date(2020, 1, 1)}
+          end={new Date(2020, 2, 2)}
+          open={true}
+          toggle={jest.fn()}
+        />
+      );
+      expect(wrapper2.find("EditPhase")).toHaveLength(1);
+    });
   });
 });
