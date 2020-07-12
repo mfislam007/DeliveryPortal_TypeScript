fb97315f993b856efadf332bf2964ba60fb44ff0
Mr Ekseli <tka@vamk.fi>
2020-07-12 11:46:47 +0300

    Added test case for some reason did not succeed with material-ui component tests

---
+++ src/components/pages/__tests__/EditPhase.test.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
+import EditPhase from "../deliveryphase/project/EditPhase";
+
+configure({ adapter: new Adapter() });
+describe('Testing AddProject component")', () => {
+  it("UI has three divs", () => {
+    const wrapper = shallow(
+      <EditPhase
+        phase="https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Installation/"
+        start={new Date(2020, 1, 1)}
+        end={new Date(2020, 2, 2)}
+        open={true}
+        toggle={null}
+      />
+    );
+    expect(wrapper.find("div")).toHaveLength(3);
+  });
+});
