ac1ed0972d52144659874f1202eac2df03b87952
Mr Ekseli <tka@vamk.fi>
2020-07-12 13:17:13 +0300

    Added test cases for EditPhase

--- src/components/pages/__tests__/EditPhase.test.tsx
+++ src/components/pages/__tests__/EditPhase.test.tsx
@@ -10,16 +10,16 @@ configure({ adapter: new Adapter() });
 describe('Testing EditPhase component")', () => {
   let render: any;
   let shallow: any;
+  const props = {
+    phase: "https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Installation/",
+    start: new Date(2020, 1, 1),
+    end: new Date(2020, 2, 2),
+    open: true,
+    toggle: jest.fn(),
+  };
+
   it("UI has three divs", () => {
-    const wrapper = shallow(
-      <EditPhase
-        phase="https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Installation/"
-        start={new Date(2020, 1, 1)}
-        end={new Date(2020, 2, 2)}
-        open={true}
-        toggle={jest.fn()}
-      />
-    );
+    const wrapper = shallow(<EditPhase {...props} />);
     expect(wrapper.find("div")).toHaveLength(3);
   });
 
@@ -30,6 +30,7 @@ describe('Testing EditPhase component")', () => {
   //https://stackoverflow.com/questions/50916374/material-ui-enzyme-testing-component
   it("Can render EditPhase", () => {
     act(() => {
+      //act need to be used when state of app changes permanantly
       const wrapper2 = render(
         <EditPhase
           phase="https://ekseli.dev.inrupt.net/private/dp2/cases/ProjectABC/Installation/"
