c1f2fa8942afb4dc4deb4b4ec7bdda934d5fd9e8
hindia <mahmmudhindia@gmail.com>
2020-07-14 14:39:07 +0300

    #321 bugfix: remov unnecessary import and renaming

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-
-import "./App.scss";
 import MenuBarSwitch from "./navigation/MenuBarSwitch";
 import HomePageSwitch from "./navigation/HomePageSwitch";
 
