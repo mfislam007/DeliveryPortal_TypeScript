1106ec8bacd18376f392e1ee29b7ad44b574aeea
Niko Siltaloppi <niko.siltaloppi@gmail.com>
2020-07-16 17:42:01 +0300

    #403 adjustments based on comments

--- src/util/controller.ts
+++ src/util/controller.ts
@@ -1,5 +1,5 @@
 export function doesFileExist(urlToFile: string): boolean {
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open("HEAD", urlToFile, false);
   xhr.send();
 
