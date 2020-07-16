364ec0127fcf74c0af1bc35e4bf05637ce20868a
hindia <mahmmudhindia@gmail.com>
2020-07-16 18:35:06 +0300

    #406 feature: get timeline data from POD

--- src/components/time-line/TimeLine.tsx
+++ src/components/time-line/TimeLine.tsx
@@ -1,149 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Timeline from "@material-ui/lab/Timeline";
 import TimelineItem from "@material-ui/lab/TimelineItem";
 import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
 import TimelineConnector from "@material-ui/lab/TimelineConnector";
 import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
-import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
 import Typography from "@material-ui/core/Typography";
 
-export default function OppositeContentTimeline() {
+import { getPhasesForProject } from "../../controllers/PhaseController";
+import Phase from "../../entities/Phase";
+import data from "../../../settings.json";
+
+import "../pages/deliveryphase/project/ProjectPage.scss";
+
+const ContentTimeline: React.FC = (): JSX.Element => {
+  const [phases, setPhases] = useState<Phase[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const phases = await getPhasesForProject(data.solid.locations.projectABC);
+      setPhases(phases);
+    })();
+  }, []);
+
+  const listPhases = phases.map((phase, index) => (
+    <TimelineItem key={index} classes={{ root: "time-line" }}>
+      <TimelineSeparator>
+        {index == 0 ? <TimelineDot color="primary" /> : <TimelineDot />}
+        {index == phases.length - 1 ? <div /> : <TimelineConnector />}
+      </TimelineSeparator>
+      <TimelineContent>
+        <Typography>{phase.name}</Typography>
+      </TimelineContent>
+      <TimelineContent>
+        <Typography>{phase.startTime.toDateString()}</Typography>
+      </TimelineContent>
+    </TimelineItem>
+  ));
+
   return (
     <React.Fragment>
-      <Timeline align="alternate">
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography>May 10</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot color="primary" />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography>Preparation</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Installation</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">May 15</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">June 15</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Milestone &#35;1</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Commissioning</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">June 30</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">July 10</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Checkpoint &#35;1</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Training</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">July 15</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">July 30</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Milestone &#35;2</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Production ramp-up</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Aug 3</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Aug 15</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Milestone &#35;3</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Checkpoint &#35;2</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Aug 30</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography color="textSecondary">Sep 1</Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography color="textSecondary">Production</Typography>
-          </TimelineContent>
-        </TimelineItem>
-      </Timeline>
+      <Timeline>{listPhases}</Timeline>
     </React.Fragment>
   );
-}
+};
+
+export default ContentTimeline;
---
+++ src/entities/Phase.ts
@@ -0,0 +1,12 @@
+class Phase {
+  constructor() {}
+  name: string;
+  startTime: Date;
+  endTime: Date;
+}
+interface Phase {
+  name: string;
+  startTime: Date;
+  endTime: Date;
+}
+export default Phase;
