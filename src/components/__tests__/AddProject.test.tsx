2f2ea36ecd13d936c00c23467b3a7bdf7979616d
Farukul Islam <mfislam2010280007@gmail.com>
2020-07-17 04:52:04 +0300

    204 Add testing case for AddProject component

--- src/components/__tests__/AddProject.test.tsx
+++ src/components/__tests__/AddProject.test.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { configure, shallow, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { configure, shallow } from "enzyme";
 import { describe, expect, it } from "@jest/globals";
 
 import AddProject from "../AddProject/AddProject";
 
-configure({ adapter: new Adapter() });
 describe("Testing for AddProject component", () => {
   it("Dialog exists", () => {
     const wrapper = shallow(<AddProject />);
